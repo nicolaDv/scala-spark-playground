6c39627eb970039fce9375420dc5d20ab6107815
ndelverm <nicola.del.verme@telenetgroup.be>
2020-12-11 11:29:39 +0100

    initial commit

--- build.sbt
+++ build.sbt
@@ -1,5 +1,13 @@
+import sbt.project
+
 name := "scala-at-light-speed"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.9"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.0.1",
+  "org.apache.spark" %% "spark-sql" % "3.0.1",
+  "io.delta" %% "delta-core" % "0.7.0"
+)
--- src/main/scala/com/rockthejvm/Playground.scala
+++ src/main/scala/com/rockthejvm/Playground.scala
@@ -1,13 +1,8 @@
 package com.rockthejvm
+import com.myCollection
 
-/**
-  * This simple application is an invitation to you to play around with any Scala code you like!
-  * (not that you'd not be able to create your own application, mind you.)
-  *
-  * So get creative - test the code that we write in the videos and try new things!
-  *
-  * Daniel @ Rock the JVM
-  */
 object Playground extends App {
-  println("I love Scala!")
+  val mySet = myCollection.Set.empty.add("bob").add("alice")
+  println(mySet("jon"))
 }
+
